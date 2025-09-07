7e5f9d564a86abd0d4fb6bd2ab4984a0c5c2a7e4
gomezalejandromatias <mati.gorriti1@gmail.com>
2025-09-07 17:13:03 -0300

    categorianegocio con codigo

--- negocio/CategoriaNegocio.cs
+++ negocio/CategoriaNegocio.cs
@@ -1,12 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using dominio;
 
 namespace negocio
 {
     internal class CategoriaNegocio
     {
+
+          
+        public List<Categoria> Categorias()
+        {
+
+            List<Categoria>lista = new List<Categoria>();
+            AccesoDatos accesoDatos = new AccesoDatos();
+
+
+            try
+            {
+                accesoDatos.SetearConsulta("select id,descripcion from CATEGORIAS");
+                accesoDatos.EjecutarLectura();
+
+                while (accesoDatos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id = (int)accesoDatos.Lector["id"];
+                    aux.Descripcion = (string)accesoDatos.Lector["descripcion"];
+
+                    lista.Add(aux);
+
+
+                }
+                return lista;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.CerrarConexion();
+
+
+            }
+        
+
+            
+
+
+
+
+
+
+
+
+         
+          
+
+
+
+
     }
 }

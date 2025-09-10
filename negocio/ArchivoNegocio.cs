4624d42526c58ef612737291a0b305bdb955ae2d
Adrian <arojasgomez.93@gmail.com>
2025-09-10 13:36:40 -0300

    Trabajo inicial rama AdrianR

--- negocio/ArchivoNegocio.cs
+++ negocio/ArchivoNegocio.cs
@@ -21,12 +21,12 @@ namespace negocio
 
             try
             {   //Comentar la que no usen 
-                //Esteban conexion a base 
-                conexion.ConnectionString = "Server=localhost,1433; Database=CATALOGO_P3_DB; Integrated Security=False; User ID=sa; Password=Esteban94*;";
+                //Esteban
+                //conexion.ConnectionString = "Server=localhost,1433; Database=CATALOGO_P3_DB; Integrated Security=False; User ID=sa; Password=Esteban94*;";
                 //Matias
 
                 //Adrian
-
+                conexion.ConnectionString = "Server=localhost,1433; Database=CATALOGO_P3_DB; Integrated Security=False; User ID=sa; Password=BaseDeDatos#2;";
                 comando.CommandType = System.Data.CommandType.Text;
                 comando.CommandText = "SELECT A.Codigo, A.Nombre, A.Descripcion, A.Precio, MIN(I.ImagenUrl) AS ImagenUrl FROM ARTICULOS A INNER JOIN IMAGENES I ON A.Id = I.IdArticulo GROUP BY A.Codigo, A.Nombre, A.Descripcion, A.Precio;";
                 comando.Connection = conexion;
